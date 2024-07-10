8db0516faa84bdeefbb7758a681429fef42a0d8e
nicolashmesias <nikemesias3@gmail.com>
2024-07-10 11:22:35 -0500

    first commit

---
+++ database/migrations/2024_06_26_142241_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('extract');
+            $table->longText('body');
+            $table->enum('status', [Post::BOORADOR,Post::PUBLICADO])->default(Post::BOORADOR)->nullable();
+            
+            $table->foreignId('category_id')->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+
+            $table->foreignId('user_id')->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
